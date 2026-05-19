ab7d3d6ea63fecc09c0500997e75e355d27033c8
aimentorstdinew <aimentorstdinew@gmail.com>
2026-05-19 20:12:15 +0530

    fgUpdate contact.mjs

--- api/contact.mjs
+++ api/contact.mjs
@@ -2,10 +2,9 @@ import { Resend } from "resend";
 
 export default async function handler(req, res) {
 try {
-
-```
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+```
 return res.status(200).json({
   success: true,
   message: "Resend imported successfully"
@@ -13,12 +12,9 @@ return res.status(200).json({
 ```
 
 } catch (error) {
-
-```
 return res.status(500).json({
-  error: error.message
+error: error.message
 });
-```
+}
+}
 
-}
-}
