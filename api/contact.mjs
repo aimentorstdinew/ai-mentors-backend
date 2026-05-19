55411c514fa7220177fb944a8a8fa3594d06425a
aimentorstdinew <aimentorstdinew@gmail.com>
2026-05-19 20:08:01 +0530

    Update contact.mjs

--- api/contact.mjs
+++ api/contact.mjs
@@ -1,35 +1,6 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export default async function handler(req, res) {
-try {
-
-```
-const result = await resend.emails.send({
-  from: "onboarding@resend.dev",
-  to: "aimentorstdi@gmail.com",
-  subject: "Static Test Email",
-  html: `
-    <h1>Resend Working Successfully</h1>
-
-    <p>Name: Nitin</p>
-
-    <p>Email: test@gmail.com</p>
-
-    <p>Message: This is a static test email.</p>
-  `,
-});
-
 return res.status(200).json({
-  success: true,
-  result,
+success: true,
+message: "Working"
 });
-```
-
-} catch (error) {
-return res.status(500).json({
-error: error.message,
-});
-}
 }
