d95536015a0820556f9f2dc1a043f940d928f2ee
aimentorstdinew <aimentorstdinew@gmail.com>
2026-05-19 20:10:03 +0530

    rrUpdate contact.mjs

--- api/contact.mjs
+++ api/contact.mjs
@@ -1,6 +1,24 @@
+import { Resend } from "resend";
+
 export default async function handler(req, res) {
+try {
+
+```
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 return res.status(200).json({
-success: true,
-message: "Working"
+  success: true,
+  message: "Resend imported successfully"
 });
+```
+
+} catch (error) {
+
+```
+return res.status(500).json({
+  error: error.message
+});
+```
+
+}
 }
